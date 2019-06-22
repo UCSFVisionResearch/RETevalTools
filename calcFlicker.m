457c1086e2251da8076c0b5795f9abe44784cf4b
Alan Kong <alan.kong@ucsf.edu>
2019-06-22 10:37:11 -0700

    FFT for flicker

--- calcFlicker.m
+++ calcFlicker.m
@@ -14,21 +14,18 @@ if ~isempty(FlickerOS)
 end
 
 %% Use FFT
-Fs= 1/(TimeOD(2)-TimeOD(1));    % Sampling freq
-T = 1/Fs;             % Sampling period
-L = length(VoltOD);   % Length of signal
+% Convert time to seconds
+if ~isempty(FlickerOD)
+    TimeSecOD = FlickerOD(:,1)/1000;
+end
+if ~isempty(FlickerOS)
+    TimeSecOS = FlickerOS(:,1)/1000;
+end
 
-Y = fft(VoltOD);
+%FFT
+AmpOD = fftRETevalAmp(TimeSecOD, VoltOD, 30);
+AmpOS = fftRETevalAmp(TimeSecOS, VoltOS, 30);
 
-% Compute the two-sided spectrum P2. Then compute the single-sided
-% spectrum P1 based on P2 and the even-valued signal length L.
-P2 = abs(Y/L);
-P1 = P2(1:L/2+1);
-P1(2:end-1) = 2*P1(2:end-1);
-
-% Define the frequency domain f and plot the single-sided amplitude spectrum P1.
-% The amplitudes are not exactly at 0.7 and 1, as expected, because of the added noise.
-f = Fs*(0:(L/2))/L;
 
 %% Split into three sections
 [pksPosOD,locsPosOD] = findpeaks(VoltOD);
