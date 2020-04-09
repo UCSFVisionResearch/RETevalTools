c24306b0d4fb448bd159e28a61698a5e6c92cfdb
Luca Della Santina <luca.dellasantina@gmail.com>
2020-04-09 12:08:40 -0700

    Second harmonic analysis
    + Calculates and plot second harmonic
    + Plots are generated and saved in landscape mode
    + Filename is displayed inside the plot
    + Clear labeling of OD/OS and common axes labels

--- calcFRC.m
+++ calcFRC.m
@@ -12,6 +12,8 @@ end
 
 freqs = [50,45,40,35,30,25,20,15,10,7,5,3,2,1,0.7,0.5,0.3];
 freqsS = {'50','45','40','35','30','25','20','15','10','7','5','3','2','1','07','05','03'};
+freqs2 = freqs*2; 
+freqs2S = {'100','90','80','70','60','50','40','30','20','14','10','6','4','2','1.4','1','0.6'};
 Sin = struct;
 
 if ~isempty(SinOD) 
@@ -44,6 +46,7 @@ clear tmp* mask Response idx
 %% Calculate Frequency Response Profile for each eye
 
 AmpsOD = zeros(size(freqs));
+AmpsODsecHarm = zeros(size(freqs));
 
 for f = 1:numel(freqs)
     Response = Sin.(['SinOD' freqsS{f} 'hz']);
@@ -54,14 +57,22 @@ for f = 1:numel(freqs)
         continue
     end
     
+    % Calculate principal harmonic amplitude from FFT
     amp = fftRETevalAmp(tmpTime', tmpVolt', freqs(f));
-        
     if ~isempty(amp)
         AmpsOD(f) = amp;
     end
+    
+    % Calculate second harmonic amplitude from FFT
+    amp2 = fftRETevalAmp(tmpTime', tmpVolt', freqs(f)*2);    
+    if ~isempty(amp2)
+        AmpsODsecHarm(f) = amp2;
+    end
+    
 end
 
 AmpsOS = zeros(size(freqs));
+AmpsOSsecHarm = zeros(size(freqs));
 
 for f = 1:numel(freqs)
     Response = Sin.(['SinOS' freqsS{f} 'hz']);
@@ -72,55 +83,93 @@ for f = 1:numel(freqs)
         continue
     end
     
-    amp = fftRETevalAmp(tmpTime', tmpVolt', freqs(f));
-        
+    % Calculate principal harmonic amplitude from FFT    
+    amp = fftRETevalAmp(tmpTime', tmpVolt', freqs(f));        
     if ~isempty(amp)
         AmpsOS(f) = amp;
     end
+    
+    % Calculate second harmonic amplitude from FFT
+    amp2 = fftRETevalAmp(tmpTime', tmpVolt', freqs(f)*2);    
+    if ~isempty(amp2)
+        AmpsOSsecHarm(f) = amp2;
+    end
+    
 end
 
 % Save data
 if ~isempty(SinOD)
     OD          = struct;
     OD.Amps     = AmpsOD;
+    OD.Amps2    = AmpsODsecHarm;
     OD.freqs    = freqs;
+    OD.freqs2   = freqs2;
 else
     SinOD = [];
     OD.Amps     = 0;
+    OD.Amps2    = 0;
     OD.freqs    = freqs;
+    OD.freqs2   = freqs2;    
 end
 
 if ~isempty(SinOS)
     OS          = struct;
     OS.Amps     = AmpsOS;
+    OS.Amps2    = AmpsOSsecHarm;
     OS.freqs    = freqs;
+    OS.freqs2   = freqs2;    
 else 
     SinOS = [];
     OS.Amps     = 0;
+    OS.Amps2    = 0;
     OS.freqs    = freqs;
+    OS.freqs2   = freqs2;    
 end
 
 % Plot Frequency Response Profile
 if exist('PlotAndSave', 'var') && PlotAndSave
-    figure('Name', 'Frequency Response Profile'); hold on;
-    subplot(1,2,1)
+    [fPath,fName,fExt] = fileparts(filename);
+    
+    figure('Name', 'Frequency Response Profile','Units', 'normalized', 'Position', [0.2 0.1 0.6 0.8], 'PaperOrientation', 'landscape'); 
+    hold on;
+    
+    t = tiledlayout(2,2);    
+    title(t, [fName fExt], 'Interpreter', 'none');
+    
+    nexttile;
     if ~isempty(SinOD)
         plot(freqs, AmpsOD, '-ok');
-        xlabel('Frequency (hz)');
-        ylabel('Amplitude (microV)');
         ylim([0 10]);
+        title('OD - Principal harmonic');
     end
     
-    subplot(1,2,2)
+    nexttile;
     if ~isempty(SinOS)
         plot(freqs, AmpsOS, '-ok');
-        xlabel('Frequency (hz)');
-        ylabel('Amplitude (microV)');
         ylim([0 10]);
+        title('OS - Principal harmonic');
     end
     
-    [filepath,name,~] = fileparts(filename);
-    print([filepath filesep name '-FRCplot.pdf'],'-dpdf','-fillpage');
-    save([filepath filesep name '-FRCdata.mat'], 'OD','OS');
+    nexttile;
+    if ~isempty(SinOD)
+        plot(freqs2, AmpsODsecHarm, '-ok');
+        ylim([0 10]);
+        title('OD - Second harmonic');
+    end
+    
+    nexttile;
+    if ~isempty(SinOS)
+        plot(freqs2, AmpsOSsecHarm, '-ok');
+        ylim([0 10]);
+        title('OS - Second harmonic');
+    end
+        
+    xlabel(t, 'Frequency (Hz)');
+    ylabel(t, 'ERG Amplitude(μV)');
+    
+    print([fPath filesep fName '-FRCplot.pdf'],'-dpdf','-fillpage');
+    save([fPath filesep fName '-FRCdata.mat'], 'OD','OS');
 end
+
+clear tmp*
 end
